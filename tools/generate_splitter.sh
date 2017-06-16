4827f99cb0c89bf96c24abdf4181917325ade5b3
Henric Andersson <ha@sensenet.nu>
2017-06-15 22:32:28 -0700

    Cleanup, removing old code
    Removed web and other old code
    Created a generator for splitter pages. Produces two pdfs, one single page and one double

---
+++ tools/generate_splitter.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+QRCODETEXT="__SCANNER_DOCUMENT_SEPARATOR__"
+QRENCODE=qrencode
+CONVERT=convert
+
+TMPFILE=/tmp/qrcode.png
+
+# Generate a QR code
+${QRENCODE} -o ${TMPFILE} "${QRCODETEXT}"
+
+# Composite a PDF
+${CONVERT} -size 1237x1762 xc:white \
+        ${TMPFILE} -gravity NorthWest -composite \
+        ${TMPFILE} -gravity North -composite \
+        ${TMPFILE} -gravity NorthEast -composite \
+        ${TMPFILE} -gravity West -composite \
+        ${TMPFILE} -gravity Center -composite \
+        ${TMPFILE} -gravity East -composite \
+        ${TMPFILE} -gravity SouthWest -composite \
+        ${TMPFILE} -gravity South -composite \
+        ${TMPFILE} -gravity SouthEast -composite \
+        -pointsize 25 \
+        -gravity Center -draw "text 0,-275 'lagerDOX'" \
+        -gravity Center -draw "text 0,-235 'http://mrworf.github.com/lagerdox'" \
+        -gravity Center -draw "text 0,-75 'Scan Once'" \
+        -gravity Center -draw "text 0,75 'Split Many'" \
+        -gravity Center -draw "text 0,275 'Place a copy of this sheet between documents to'" \
+        -gravity Center -draw "text 0,315 'split scanned items into multiple PDFs'" \
+        -gravity Center -draw "text 0,415 'Tip!'" \
+        -gravity Center -draw "text 0,455 'Print this double-sided'" \
+        -compress ZIP \
+        pdf:single.pdf
+
+# Generate double sided version
+${CONVERT} single.pdf single.pdf -compress ZIP double.pdf
